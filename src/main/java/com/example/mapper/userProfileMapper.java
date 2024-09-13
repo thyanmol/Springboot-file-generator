bf131e3c40ae397fcd9f60b72066efeae822e694
Anmol Jain <anmoljain232606@gmail.com>
2024-09-14 01:22:53 +0530

    Initial Commit

--- src/main/java/com/example/mapper/userProfileMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.mapper;
-
-import com.example.domain.userProfile;
-import com.example.service.dto.userProfileDTO;
-import com.example.service.mapper.EntityMapper;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface userProfileMapper extends EntityMapper<userProfileDTO, userProfile> {}
--- src/main/java/com/example/repository/userProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repository;
-
-import com.example.domain.userProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface userProfileRepository extends JpaRepository<userProfile, Long> {}
